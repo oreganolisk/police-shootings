d2052d578a44f9417612c1978ac5e6ae070cdcb4
anon1 <anon1@nowhere>
2020-06-03 10:02:28 -0700

    re-roll, change db schema

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,59 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const db = require('./db');
+const db: {
+  rows: {
+    id: string,
+    race: string,
+    armed: boolean
+  }[]
+} = require('./db');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+function getRandomDbIndex() {
+  return Math.floor(Math.random() * db.rows.length);
+}
+
+interface AppProps {
+}
+
+interface AppState {
+  dbIndex: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      dbIndex: getRandomDbIndex()
+    };
+  }
+
+  public render() {
+    const data = db.rows[this.state.dbIndex];
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Random Violence
+          </p>
+          <button className="App-button" onClick={() => this.reroll()}>
+            Re-roll
+          </button>
+        </header>
+        <main>
         <p>
-          {Object.keys(db).map(key => {
-            return <p>{key} - {JSON.stringify(db[key])}</p>
-          })}
-        </p>
-      </header>
-    </div>
-  );
+            {data.id} - race: {data.race} armed: {data.armed ? "yes" : "no"}
+          </p>
+        </main>
+      </div>
+    );
+  }
+
+  private reroll() {
+    this.setState({
+      dbIndex: getRandomDbIndex()
+    });
+  }
 }
 
 export default App;

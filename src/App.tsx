3c956f16c89739ad13aa0cd95f253647bf256c03
anon1 <anon1@nowhere>
2020-06-03 10:47:55 -0700

    load content blobs on demand

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
-const db: {
-  rows: {
-    id: string,
-    race: string,
-    armed: boolean
-  }[]
-} = require('./db');
+interface IncidentMetadata {
+  id: string,
+  race: string,
+  armed: boolean
+}
+
+interface IncidentData {
+  name: string;
+  age: number;
+}
+
+const db: { rows: IncidentMetadata[] } = require('./db');
 
 function getRandomDbIndex() {
   return Math.floor(Math.random() * db.rows.length);
+}
+
+async function loadIncidentData(id: string): Promise<IncidentData> {
+  const data = await axios.get(`db/${id}.json`);
+  return data.data as IncidentData;
 }
 
 interface AppProps {
 }
 
 interface AppState {
-  dbIndex: number;
+  metadata: IncidentMetadata | null;
+  data: IncidentData | null;
 }
 
 class App extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
     this.state = {
-      dbIndex: getRandomDbIndex()
+      metadata: null,
+      data: null
     };
   }
 
+  public componentDidMount() {
+    this.reload();
+  }
+
   public render() {
-    const data = db.rows[this.state.dbIndex];
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Random Violence
-          </p>
-          <button className="App-button" onClick={() => this.reroll()}>
-            Re-roll
-          </button>
-        </header>
-        <main>
-        <p>
-            {data.id} - race: {data.race} armed: {data.armed ? "yes" : "no"}
-          </p>
-        </main>
+        {this.renderHeader()}
+        {this.renderMain()}
       </div>
     );
   }
 
-  private reroll() {
-    this.setState({
-      dbIndex: getRandomDbIndex()
-    });
+  private renderHeader(): JSX.Element {
+    return <header className="App-header">
+        <p>
+          Random Violence
+        </p>
+        <button className="App-button" onClick={() => this.reload()}>
+          Reload
+        </button>
+    </header>
+  }
+
+  private renderMain(): JSX.Element {
+    const meta = this.state.metadata;
+    const data = this.state.data;
+    if (meta && data) {
+    return <main>
+      <div>
+        <p>id: {meta.id}</p>
+        <p>race: {meta.race}</p>
+        <p>armed: {meta.armed ? "yes" : "no"}</p>
+        <p>name: {data.name}</p>
+        <p>age: {data.age}</p>
+      </div>
+    </main>
+    } else {
+      return <main>Loading...</main>
+    }
+  }
+
+  private async reload() {
+    const dbIndex = getRandomDbIndex();
+    const metadata = db.rows[dbIndex];
+    const data = await loadIncidentData(metadata.id);
+    this.setState({ metadata, data });
   }
 }
 
